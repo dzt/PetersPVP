1f371ad6e5c87f87dedacf1d88276fe672da3d21
Peter Soboyejo <thepcmrtim@gmail.com>
2016-06-29 01:56:44 -0400

    init with a bunch of stuff

---
+++ gamemode/shared.lua
@@ -0,0 +1,8 @@
+GM.Name = "PeterPVP"
+GM.Author = "Peter Soboyejo<thepcmrtim@gmail.com>"
+GM.Email = "thepcmrtim@gmail.com"
+GM.Website = "http://petersoboyejo.com/"
+
+function GM:Initialize()
+	
+end
